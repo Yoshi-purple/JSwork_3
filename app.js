cc7e7e6f10c16bb5045205eef1bed0942f1e77ff
Yoshi-purple <n7ydahew@gmail.com>
2021-01-05 09:52:08 +0900

    JS課題③-4ラジオボタンの振り分けの実装をしました。

--- app.js
+++ app.js
@@ -6,10 +6,18 @@
   //要素の取得
   const todoList = document.getElementById('todo-list');
   const taskInput = document.getElementById('taskInput');
-
+  //追加ボタンの取得
   const btnAdd = document.getElementById('btnAdd');
   btnAdd.addEventListener('click', addList);
+  //ラジオボタンの取得
+  const allTask = document.getElementById('allTask');
+  allTask.onclick = radioSwitchAll;
+  const workingTask = document.getElementById('workingTask');
+  workingTask.onclick = radioSwitchWork;
+  const completeTask = document.getElementById('completeTask');
+  completeTask.onclick = radioSwitchComp;
 
+  //配列
   let newTodo = [{ id: 'ID', comment: 'コメント', status: '状態' }];
 
   //最初の ID コメント　状態の表示
@@ -27,12 +35,16 @@
   function addList() {
     const todoLi = document.createElement('li');
     todoLi.id = 'todo-li';
+    todoLi.className = 'working';
+    // const todoDiv = document.createElement('div');
+    // todoDiv.className = 'working';
     const newId = document.createElement('span');
     newId.classList.add('num-id');
     const newComment = document.createElement('span');
     newComment.classList.add('todo-comment');
     const btnStatus = document.createElement('button');
     btnStatus.innerText = '作業中';
+    // btnStatus.className = 'working';
     btnStatus.onclick = checkStatus;
     const btnDelete = document.createElement('button');
     btnDelete.innerText = '削除';
@@ -56,6 +68,7 @@
       todoLi.appendChild(newComment);
       todoLi.appendChild(btnStatus);
       todoLi.appendChild(btnDelete);
+      // todoLi.appendChild(todoDiv);
       todoList.appendChild(todoLi);
     }
 
@@ -82,13 +95,29 @@
     }
   }
 
+  //ボタンの切り替えの関数
   function checkStatus(e) {
     let targetBtn = e.target;
-    const _btnSts = document.getElementById('btn-working');
-    if (targetBtn.innerText === '作業中') {
+    if (targetBtn.parentElement.className === 'working') {
       targetBtn.innerText = '完了';
+
+      targetBtn.parentElement.classList.add('complete');
+      targetBtn.parentElement.classList.remove('working');
     } else {
       targetBtn.innerText = '作業中';
+
+      targetBtn.parentElement.classList.add('working');
+      targetBtn.parentElement.classList.remove('complete');
     }
   }
+
+  //ラジオボタンによる切り替えの関数
+  //すべて
+  function radioSwitchAll() {}
+
+  //作業中
+  function radioSwitchWork() {}
+
+  //完了
+  function radioSwitchComp() {}
 }
